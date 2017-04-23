4d8215a9bb143d374f49b747922490efb8b30081
DGitH <ghwns9652@dgist.ac.kr>
2017-04-23 20:49:16 +0900

    Update Res_class.cpp

--- Res_class.cpp
+++ Res_class.cpp
@@ -1,5 +1,15 @@
 struct STAGE_RES
 {
-public:
-
+  int opcode;
+  int rs;
+  int rt;
+  int rd;
+  int shift;
+  int funct;
+  int reg_wt;
+  int PC;
+  int mem_wt;
+  int mem_rd;
+  int sign_ex;
+  int ALUSrc;
 };
