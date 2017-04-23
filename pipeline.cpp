6de70d3b31485aa41d7ed1c799bd8e9f5391e2e3
DGitH <ghwns9652@dgist.ac.kr>
2017-04-23 20:55:57 +0900

    Update pipeline.cpp

--- pipeline.cpp
+++ pipeline.cpp
@@ -20,6 +20,26 @@ int MEM_WB_Res[20];
 
 string sample = "00000000000000000000000000000000";
 
+
+struct STAGE_RES
+{
+  int opcode;
+  int rs;
+  int rt;
+  int rd;
+  int shift;
+  int funct;
+  int reg_wt;
+  int PC;
+  int mem_wt;
+  int mem_rd;
+  int sign_ex;
+  int ALUSrc;
+  int branch;
+  int jump;
+  int Regdst;
+};
+
 //addu 명령어 실행해보기
 
 int type_checker(int op)
