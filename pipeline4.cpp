bdb1ae9ca8735d6fe42de3d85811f5b3fed089f2
DGitH <ghwns9652@dgist.ac.kr>
2017-04-29 17:16:27 +0900

    Update pipeline4.cpp

--- pipeline4.cpp
+++ pipeline4.cpp
@@ -1140,7 +1140,24 @@ int run_bin(int num_instruc, int d_exist, unsigned int* memory_range) {
 		if (stage_state[0] = 1) {
 			IF_ID = IF();
 		}
-
+		
+		if (MEM_WB.flush == 3) {
+			EX_MEM = STAGE_REG();
+			ID_EX = STAGE_REG();
+			IF_ID = STAGE_REG();
+			if (MEM_WB.NPC - 4 == 0x400000 + text_size) {
+				stage_state[0] = 1;
+				stage_state[1] = 1;
+				stage_state[2] = 1;
+			}
+		}
+		if (ID_EX.flush = 1) {
+			IF_ID = STAGE_REG();
+			if (ID_EX.NPC - 4 == 0x400000 + text_size) {
+				stage_state[0] = 1;
+			}
+		}
+		
 		/*bin_parser(str_line);*/
 		if (d_exist) {
 			if (!(0x400000 <= PC && PC < (0x400000 + text_size))) {
